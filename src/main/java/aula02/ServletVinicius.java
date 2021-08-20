162701d3bacebff4ece8a62ecdf89543aadb0081
vinicius-carv <valves8956@gmail.com>
2021-08-19 22:24:58 -0300

    Atividade 02

--- src/main/java/aula02/ServletVinicius.java
+++ src/main/java/aula02/ServletVinicius.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ServletFelipeLima
+ * Servlet implementation class ServletVinicius
  */
-@WebServlet("/ServletFelipeLima")
-public class ServletFelipeLima extends HttpServlet {
+@WebServlet("/ServletVinicius")
+public class ServletVinicius extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletFelipeLima() {
+    public ServletVinicius() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +27,7 @@ public class ServletFelipeLima extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Felipe Ferreira Lima e Lima");
+		response.getWriter().append("Vinícius Alves de Carvalho");
 	}
 
 }
